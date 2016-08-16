e7332277f4f2f8c75e68efb78a54b44951d4046a
ndehio <ndehio@techfak.uni-bielefeld.de>
2016-08-16 14:22:43 +0200

    added nullspace-controller

---
+++ src/nullspace-controller/NullspaceController.cpp
@@ -0,0 +1,113 @@
+/* Author: Niels Dehio
+ * Date:   16 August 2016
+ *
+ * Description: 
+ */
+
+#include "NullspaceController.hpp"
+#include <rtt/Component.hpp> // needed for the macro at the end of this file
+
+
+NullspaceController::NullspaceController(std::string const & name) : RTT::TaskContext(name) {
+    //prepare operations
+    addOperation("setDOFsize", &NullspaceController::setDOFsize, this).doc("set DOF size");
+
+    //other stuff
+    gainP = 1;
+    gainD = 0;
+    portsArePrepared = false;
+}
+
+bool NullspaceController::configureHook() {
+    return true;
+}
+
+bool NullspaceController::startHook() {
+    return true;
+}
+
+void NullspaceController::updateHook() {
+    //update desired angles only when new weight-vector is received
+    if (in_desiredAngles_port.connected()) {
+        in_desiredAngles_flow = in_desiredAngles_port.read(in_desiredAngles_var);
+        if (in_desiredAngles_flow == RTT::NewData){
+            current_desiredAngles = in_desiredAngles_var;
+        }
+    }
+
+    in_robotstatus_flow = in_robotstatus_port.read(in_robotstatus_var);
+    in_jacobian_flow = in_jacobian_port.read(in_jacobian_var);
+    in_jacobianInv_flow = in_jacobianInv_port.read(in_jacobianInv_var);
+
+    out_torques_var.torques.setZero();
+    out_torques_var.torques = (identityDOFsizeDOFsize - in_jacobian_var.transpose() * in_jacobianInv_var);
+    out_torques_var.torques *= (gainP * (in_desiredAngles_var.angles - in_robotstatus_var.angles) - gainD * in_robotstatus_var.velocities );
+    out_torques_port.write(out_torques_var);
+}
+
+void NullspaceController::stopHook() {
+}
+
+void NullspaceController::cleanupHook() {
+    portsArePrepared = false;
+}
+
+void NullspaceController::setDOFsize(unsigned int DOFsize){
+    assert(DOFsize > 0);
+    this->DOFsize = DOFsize;
+    this->identityDOFsizeDOFsize = Eigen::MatrixXf(DOFsize,DOFsize);
+    this->current_desiredAngles = rstrt::kinematics::JointAngles(DOFsize);
+    this->preparePorts();
+}
+
+void NullspaceController::preparePorts(){
+    if (portsArePrepared){
+        ports()->removePort("in_robotstatus_port");
+        ports()->removePort("in_desiredAngles_port");
+        ports()->removePort("in_jacobian_port");
+        ports()->removePort("in_jacobianInv_port");
+        ports()->removePort("out_torques_port");
+    }
+
+    //prepare input
+    in_robotstatus_var = rstrt::robot::JointState(DOFsize);
+    in_robotstatus_port.setName("in_robotstatus_port");
+    in_robotstatus_port.doc("Input port for reading robotstatus values");
+    ports()->addPort(in_robotstatus_port);
+    in_robotstatus_flow = RTT::NoData;
+
+    in_desiredAngles_var = rstrt::kinematics::JointAngles(DOFsize);
+    in_desiredAngles_port.setName("in_desiredAngles_port");
+    in_desiredAngles_port.doc("Input port for reading desired angles values");
+    ports()->addPort(in_desiredAngles_port);
+    in_desiredAngles_flow = RTT::NoData;
+
+    in_jacobian_var = Eigen::MatrixXf(6,DOFsize);
+    in_jacobian_port.setName("in_jacobian_port");
+    in_jacobian_port.doc("Input port for reading jacobian values");
+    ports()->addPort(in_jacobian_port);
+    in_jacobian_flow = RTT::NoData;
+
+    in_jacobianInv_var = Eigen::MatrixXf(DOFsize,6);
+    in_jacobianInv_port.setName("in_jacobianInv_port");
+    in_jacobianInv_port.doc("Input port for reading jacobian inverse values");
+    ports()->addPort(in_jacobianInv_port);
+    in_jacobianInv_flow = RTT::NoData;
+
+    //prepare output
+    out_torques_var = rstrt::dynamics::JointTorques(DOFsize);
+    out_torques_var.torques.setZero();
+    out_torques_port.setName("out_torques_port");
+    out_torques_port.doc("Output port for sending torque values");
+    out_torques_port.setDataSample(out_torques_var);
+    ports()->addPort(out_torques_port);
+
+    portsArePrepared = true;
+}
+
+
+//this macro should appear only once per library
+//ORO_CREATE_COMPONENT_LIBRARY()
+
+// This macro, as you can see, creates the component. Every component should have this!
+ORO_LIST_COMPONENT_TYPE(NullspaceController)
---
+++ src/nullspace-controller/NullspaceController.hpp
@@ -0,0 +1,61 @@
+/* Author: Niels Dehio
+ * Date:   16 August 2016
+ *
+ * Description: 
+ */
+
+#pragma once
+
+#include <rtt/Port.hpp>
+#include <rtt/TaskContext.hpp>
+#include <string>
+
+#include <rst-rt/robot/JointState.hpp>
+#include <rst-rt/dynamics/JointTorques.hpp>
+#include <rst-rt/kinematics/JointAngles.hpp>
+#include <rst-rt/kinematics/JointVelocities.hpp>
+
+class NullspaceController: public RTT::TaskContext {
+public:
+    NullspaceController(std::string const & name);
+
+    // RTT::TaskContext methods that are needed for any standard component and
+    // should be implemented by user
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+    void stopHook();
+    void cleanupHook();
+
+    void setDOFsize(unsigned int DOFsize);
+    void preparePorts();
+
+private:
+    // Declare input ports and their datatypes
+    RTT::InputPort<rstrt::robot::JointState> in_robotstatus_port;
+    RTT::InputPort<rstrt::kinematics::JointAngles> in_desiredAngles_port;
+    RTT::InputPort<Eigen::MatrixXf> in_jacobian_port;
+    RTT::InputPort<Eigen::MatrixXf> in_jacobianInv_port;
+
+    // Declare output ports and their datatypes
+    RTT::OutputPort<rstrt::dynamics::JointTorques> out_torques_port;
+
+    // Data flow:
+    RTT::FlowStatus in_robotstatus_flow;
+    RTT::FlowStatus in_desiredAngles_flow;
+    RTT::FlowStatus in_jacobian_flow;
+    RTT::FlowStatus in_jacobianInv_flow;
+
+    // variables
+    rstrt::robot::JointState in_robotstatus_var;
+    rstrt::kinematics::JointAngles in_desiredAngles_var;
+    Eigen::MatrixXf in_jacobian_var;
+    Eigen::MatrixXf in_jacobianInv_var;
+    rstrt::dynamics::JointTorques out_torques_var;
+    Eigen::MatrixXf identityDOFsizeDOFsize;
+    rstrt::kinematics::JointAngles current_desiredAngles;
+    unsigned int DOFsize;
+    float gainP, gainD;
+    bool portsArePrepared;
+};
+
