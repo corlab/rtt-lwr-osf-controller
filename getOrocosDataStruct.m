0bee46cc427161ff502df45972be134b400a10a5
ndehio <ndehio@online.de>
2016-08-24 20:57:58 +0200

    improved matlab scripts

--- getOrocosDataStruct.m
+++ getOrocosDataStruct.m
@@ -1,23 +1,23 @@
-function [report skip]= getOrocosDataStruct1(filename, numjoints)
-    [data skip] = readOrocosData(filename, 2+0*numjoints+5*3);
+function [report skip]= getOrocosDataStruct1(filename, numjoints, taskSpaceDim)
     joints = 1:numjoints;
-    cartesian = 1:3;
+    taskSpaceArray = 1:taskSpaceDim;
+    [data skip] = readOrocosData(filename, 2+0*numjoints+5*taskSpaceDim);
     
     report = struct;
     report.data = data;
     idx=0;
-    report.timestamps                      = data(:,1+idx);%special
+    report.timestamps           = data(:,1+idx);%special
     idx=idx+1;
-    report.curCartPosTaskTranslation       = data(:,cartesian+idx);
-    idx=idx+3;
-    report.curCartVelTaskTranslation       = data(:,cartesian+idx);
-    idx=idx+3;
-    report.desCartPosTaskTranslation       = data(:,cartesian+idx);
-    idx=idx+3;
-    report.desCartVelTaskTranslation       = data(:,cartesian+idx);
-    idx=idx+3;
-    report.desCartAccTaskTranslation       = data(:,cartesian+idx);
-    idx=idx+3;
+    report.curCartPosTask       = data(:,taskSpaceArray+idx);
+    idx=idx+taskSpaceDim;
+    report.curCartVelTask       = data(:,taskSpaceArray+idx);
+    idx=idx+taskSpaceDim;
+    report.desCartPosTask       = data(:,taskSpaceArray+idx);
+    idx=idx+taskSpaceDim;
+    report.desCartVelTask       = data(:,taskSpaceArray+idx);
+    idx=idx+taskSpaceDim;
+    report.desCartAccTask       = data(:,taskSpaceArray+idx);
+    idx=idx+taskSpaceDim;
     
     assert(idx+1==size(data,2)); %+1 because of last space as senseless variable
 end
